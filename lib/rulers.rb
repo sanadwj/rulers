b1b3f52fa5694b17171f8ff66c49917d55125af5
Sanad <s_wj_88@hotmail.com>
2021-06-14 04:05:30 +0300

    Add test_helper
    Add rack test

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
-
+require 'rulers/array'
 require_relative "rulers/version"
 
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`
       [200, {'Content-type' => 'text/html'}, ["hello from ruby on rulers"]]
     end
   end
