e7566f8d17d5c98d0e68811f73c3e8a11c9906c9
Sanad <s_wj_88@hotmail.com>
2021-06-14 07:11:54 +0300

    Modify rulers.rb

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
-require 'rulers/array'
 require_relative "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [200, {'Content-type' => 'text/html'}, ["hello from ruby on rulers"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        return [200, {'Content-Type' => 'text/html'}, ['quotes/a_quote']]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
+
+
