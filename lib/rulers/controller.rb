237a9d63fb9b83a5f6dc38952bf82a83c94fa7e5
Sanad <s_wj_88@hotmail.com>
2021-06-19 12:49:21 +0300

    Define controller_name method to add it to the views

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -10,8 +10,14 @@ module Rulers
       @env
     end
 
+    def controller_nam
+      klass = self.class
+      klass = klass.to_s.gsub(/Controller$/,"")
+      Rulers.to_underscore(klass)
+    end
+
     def render(view_name, locals = {})
-      filename = File.join "app", "views","#{view_name}.html.erb"
+      filename = File.join "app", "views",controller_nam, "#{view_name}.html.erb"
       template = File.read(filename)
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(:env => env)
